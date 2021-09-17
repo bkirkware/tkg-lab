939fee618b61ee5a7ce541b5c74d72de660ed7f9
Brian Kirkland <bkirkland@pivotal.io>
2021-09-17 17:02:56 -0400

    uncomment

--- scripts/deploy-all-workload-cluster-components.sh
+++ scripts/deploy-all-workload-cluster-components.sh
@@ -4,21 +4,21 @@ TKG_LAB_SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
 source $TKG_LAB_SCRIPTS/set-env.sh
 
 # Workload Step 1
-#$TKG_LAB_SCRIPTS/deploy-workload-cluster.sh \
-#  $(yq e .workload-cluster.name $PARAMS_YAML) \
-#  $(yq e .workload-cluster.worker-replicas $PARAMS_YAML) \
-#  $(yq e .workload-cluster.controlplane-endpoint $PARAMS_YAML) \
-#  $(yq e '.shared-services-cluster.kubernetes-version // null' $PARAMS_YAML)
-## Workload Step 2
-#$TKG_LAB_SCRIPTS/tmc-attach.sh $(yq e .workload-cluster.name $PARAMS_YAML)
-## Workload Step 3
-#$TKG_LAB_SCRIPTS/tmc-policy.sh \
-#  $(yq e .workload-cluster.name $PARAMS_YAML) \
-#  cluster.admin \
-#  platform-team
-## Workload Step 4
-#IAAS=$(yq e .iaas $PARAMS_YAML)
-#$TKG_LAB_SCRIPTS/deploy-cert-manager.sh $(yq e .workload-cluster.name $PARAMS_YAML)
+$TKG_LAB_SCRIPTS/deploy-workload-cluster.sh \
+  $(yq e .workload-cluster.name $PARAMS_YAML) \
+  $(yq e .workload-cluster.worker-replicas $PARAMS_YAML) \
+  $(yq e .workload-cluster.controlplane-endpoint $PARAMS_YAML) \
+  $(yq e '.shared-services-cluster.kubernetes-version // null' $PARAMS_YAML)
+# Workload Step 2
+$TKG_LAB_SCRIPTS/tmc-attach.sh $(yq e .workload-cluster.name $PARAMS_YAML)
+# Workload Step 3
+$TKG_LAB_SCRIPTS/tmc-policy.sh \
+  $(yq e .workload-cluster.name $PARAMS_YAML) \
+  cluster.admin \
+  platform-team
+# Workload Step 4
+IAAS=$(yq e .iaas $PARAMS_YAML)
+$TKG_LAB_SCRIPTS/deploy-cert-manager.sh $(yq e .workload-cluster.name $PARAMS_YAML)
 $TKG_LAB_SCRIPTS/generate-and-apply-contour-yaml.sh $(yq e .workload-cluster.name $PARAMS_YAML)
 $TKG_LAB_SCRIPTS/generate-and-apply-external-dns-yaml.sh \
   $(yq e .workload-cluster.name $PARAMS_YAML) \
